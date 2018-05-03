324a391a594ead994469d4f9ce25dbb541a5e5e3
XtraAzure <35952292+XtraAzure@users.noreply.github.com>
2018-05-04 02:48:20 +0900

    Add files via upload

---
+++ 1-5 Dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )");
+	printf("\"\"\"\\");
+	printf("\n");
+	printf("|\"\^\"\`    |");
+	printf("\n");
+	printf("||_/=\\\\__|");
+	printf("\n");
+
+	//system("PAUSE");
+	return 0;
+}
